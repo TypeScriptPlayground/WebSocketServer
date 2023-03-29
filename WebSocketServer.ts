e3d9e99be4d6c0de674c3d5a3d950ab50351e59d
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-03-29 15:36:12 +0200

    Update WebSocketServer.ts

--- WebSocketServer.ts
+++ WebSocketServer.ts
@@ -10,3 +10,37 @@ export function WebSocketServer(options : Deno.ServeOptions | Deno.ServeTlsOptio
         return response
     })
 }
+
+// export class WebSocketServer {
+//     public CLOSED : number
+//     public CLOSING : number
+//     public CONNECTING : number
+//     public OPEN : number
+
+//     public binaryType : BinaryType
+//     public bufferedAmount : number
+//     public extensions : string
+//     public protocol : string
+//     public readyState : number
+//     public url : string
+
+//     public onclose : ((this: WebSocket, ev: CloseEvent) => any) | null
+//     public onerror : ((this: WebSocket, ev: Event | ErrorEvent) => any) | null
+//     public onmessage : ((this: WebSocket, ev: MessageEvent<any>) => any) | null
+//     public onopen : ((this: WebSocket, ev: Event) => any) | null
+
+//     addEventListener<K extends keyof WebSocketEventMap>(
+//         type : K | string,
+//         listener : (this : WebSocket, ev : WebSocketEventMap[K]) => any | EventListenerOrEventListenerObject,
+//         options? : boolean | AddEventListenerOptions,
+//     ) : void {};
+//     removeEventListener<K extends keyof WebSocketEventMap>(
+//         type : K | string,
+//         listener : (this: WebSocket, ev : WebSocketEventMap[K]) => any | EventListenerOrEventListenerObject,
+//         options? : boolean | EventListenerOptions,
+//     ) : void {};
+//     dispatchEvent(event : Event) : boolean {};
+
+//     send(data : string | ArrayBufferLike | Blob | ArrayBufferView) : void {};
+//     close(code? : number, reason? : string) : void {};
+// }
